221d34f5976fd6de3b77577b1a22e443fc9a15e8
Renan <souza.rnn@gmail.com>
2022-07-11 17:22:00 -0300

    Project started

---
+++ backend/src/test/java/com/renan/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
